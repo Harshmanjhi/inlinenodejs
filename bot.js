d2c5ec7158fed8aaef65c84f2a3869fb277d29f9
Harsh123op <harshmanjhi180+1@gmail.com>
2024-10-23 22:44:29 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -519,7 +519,7 @@ bot.on('inline_query', (ctx) => inlineQuery(ctx)); // Modify this line
 bot.on('message', messageCounter);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile('index.html');
 });
 
 // Start the Express server with the hardcoded port
