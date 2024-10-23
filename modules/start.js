05132fc644e19f8d8875b1fb6977f0c7e6196d72
Harsh123op <harshmanjhi180+1@gmail.com>
2024-10-24 00:41:24 +0530

    Update start.js

--- modules/start.js
+++ modules/start.js
@@ -92,7 +92,6 @@ const start = async (ctx) => {
     await ctx.telegram.editMessageText(checkMsg.chat.id, checkMsg.message_id, null, "🌟");
 
     // Final step: Send the main message
-    const photoUrl = PHOTO_URL[Math.floor(Math.random() * PHOTO_URL.length)];
     const keyboard = Markup.inlineKeyboard([
         [Markup.button.url("🎭 ADD ME TO YOUR GROUP 🎭", `http://t.me/${BOT_USERNAME}?startgroup=new`)],
         [Markup.button.url("💬 SUPPORT", `https://t.me/${SUPPORT_CHAT}`),
@@ -101,7 +100,7 @@ const start = async (ctx) => {
 
     await ctx.telegram.sendPhoto(
         ctx.chat.id,
-        photoUrl,
+        PHOTO_URL,
         {
             caption: `
 🎮 ***Welcome to Epic Arena!***
